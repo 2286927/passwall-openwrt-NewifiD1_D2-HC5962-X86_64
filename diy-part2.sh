1b02c54dca9adb2b70a6811085aea459bff98c7c
ChenChen <cc2286927@sina.cn>
2022-06-04 16:52:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -6,3 +6,6 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
+# zerotier
+sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.8.4/g' feeds/packages/net/zerotier/Makefile
+#sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.8.4/g' /openwrt/feeds/packages/net/zerotier/Makefile
