2459c6895819b6a747a11c6777c881f982d9dc2e
ChenChen <cc2286927@sina.cn>
2022-09-07 08:52:34 +0800

    Create diy-part2-hc5962.sh

---
+++ DIY/diy-part2-hc5962.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+sed -i 's/192.168.1.1/9.1.1.1/g' package/base-files/files/bin/config_generate
+# DNS Cache FixUP
+echo -e "#max-ttl=600\nneg-ttl=600\nmin-cache-ttl=3600" >> package/network/services/dnsmasq/files/dnsmasq.conf
+###### Modify hostname
+sed -i 's/OpenWrt/HiWiFi4E-one/g' package/base-files/files/bin/config_generate
+####### Modify the version number
+sed -i "s/OpenWrt /Fndsz build from Lede $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+# zerotier1.8.4
+sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.8.4/g' feeds/packages/net/zerotier/Makefile
+#sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.8.4/g' /openwrt/feeds/packages/net/zerotier/Makefile
