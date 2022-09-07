e1fd6a00c7eaee606becd35148496785ffca09d6
ChenChen <cc2286927@sina.cn>
2022-09-07 08:32:47 +0800

    Create diy-part2-d1.sh

---
+++ DIY/diy-part2-d1.sh
@@ -0,0 +1,8 @@
+# Modify default IP
+sed -i 's/192.168.1.1/3.1.1.1/g' package/base-files/files/bin/config_generate
+# DNS Cache FixUP
+echo -e "#max-ttl=600\nneg-ttl=600\nmin-cache-ttl=3600" >> package/network/services/dnsmasq/files/dnsmasq.conf
+###### Modify hostname
+sed -i 's/OpenWrt/NewifiD1/g' package/base-files/files/bin/config_generate
+####### Modify the version number
+sed -i "s/OpenWrt /Fndsz build from Lede $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
