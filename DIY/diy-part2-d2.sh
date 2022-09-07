a4bd321655560ebe43ba20dbd4579338b45a883b
ChenChen <cc2286927@sina.cn>
2022-09-07 08:37:07 +0800

    Create diy-part2-d2.sh

---
+++ DIY/diy-part2-d2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+sed -i 's/192.168.1.1/6.1.1.1/g' package/base-files/files/bin/config_generate
+# DNS Cache FixUP
+echo -e "#max-ttl=600\nneg-ttl=600\nmin-cache-ttl=3600" >> package/network/services/dnsmasq/files/dnsmasq.conf
