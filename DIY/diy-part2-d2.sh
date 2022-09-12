f31e84a6b5cc27642e801d67fb353ab1a04579ad
ChenChen <cc2286927@sina.cn>
2022-09-13 06:37:53 +0800

    Update diy-part2-d2.sh

--- DIY/diy-part2-d2.sh
+++ DIY/diy-part2-d2.sh
@@ -10,9 +10,9 @@ sed -i 's/192.168.1.1/6.1.1.1/g' package/base-files/files/bin/config_generate
 # DNS Cache FixUP
 echo -e "#max-ttl=600\nneg-ttl=600\nmin-cache-ttl=3600" >> package/network/services/dnsmasq/files/dnsmasq.conf
 ###### Modify hostname
-sed -i 's/OpenWrt/NewifiD2/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Newifi-D2/g' package/base-files/files/bin/config_generate
 ####### Modify the version number
 sed -i "s/OpenWrt /Fndsz build from Lede $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 # zerotier1.8.4
-sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.8.4/g' feeds/packages/net/zerotier/Makefile
+#sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.8.4/g' feeds/packages/net/zerotier/Makefile
 #sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.8.4/g' /openwrt/feeds/packages/net/zerotier/Makefile
