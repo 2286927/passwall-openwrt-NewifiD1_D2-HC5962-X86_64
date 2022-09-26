9ed2eabb2c5e694e39131daf2164baeab7b5bb1e
ChenChen <cc2286927@sina.cn>
2022-09-26 17:15:20 +0800

    Update diy-part2-hc5962-One.sh

--- DIY/diy-part2-hc5962-One.sh
+++ DIY/diy-part2-hc5962-One.sh
@@ -14,5 +14,5 @@ sed -i 's/OpenWrt/HiWiFi4E-one/g' package/base-files/files/bin/config_generate
 ####### Modify the version number
 sed -i "s/OpenWrt /Fndsz build from Lienol $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/default-settings/files/zzz-default-settings
 # zerotier1.8.4
-sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.8.4/g' feeds/packages/net/zerotier/Makefile
+# sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.8.4/g' feeds/packages/net/zerotier/Makefile
 #sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.8.4/g' /openwrt/feeds/packages/net/zerotier/Makefile
