d97754d96b6449414d7b1b472fe3a5ee7248d874
ChenChen <cc2286927@sina.cn>
2022-11-01 11:41:57 +0800

    Update diy-part2-d1.sh

--- DIY/diy-part2-d1.sh
+++ DIY/diy-part2-d1.sh
@@ -1,10 +1,10 @@
 # Modify default IP
-sed -i 's/192.168.1.1/3.1.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
 # DNS Cache FixUP
 echo -e "#max-ttl=600\nneg-ttl=600\nmin-cache-ttl=3600" >> package/network/services/dnsmasq/files/dnsmasq.conf
 ###### Modify hostname
 sed -i 's/OpenWrt/Newifi-D1/g' package/base-files/files/bin/config_generate
-
+sed -i 's/immortalwrt/Newifi-D1/g' package/base-files/files/bin/config_generate
 ####### Modify the Lienol version number
 #sed -i "s/OpenWrt /洲•Cy build from Lienol $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/default-settings/files/zzz-default-settings
 ####### Modify the version number
