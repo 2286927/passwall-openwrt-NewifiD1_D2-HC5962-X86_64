fcce3028e347dd557743345c1b695f55308ee954
ChenChen <cc2286927@sina.cn>
2022-11-01 20:43:34 +0800

    Update diy-part2-hc5962.sh

--- DIY/diy-part2-hc5962.sh
+++ DIY/diy-part2-hc5962.sh
@@ -10,7 +10,7 @@ sed -i 's/192.168.1.1/9.1.1.1/g' package/base-files/files/bin/config_generate
 # DNS Cache FixUP
 echo -e "#max-ttl=600\nneg-ttl=600\nmin-cache-ttl=3600" >> package/network/services/dnsmasq/files/dnsmasq.conf
 ###### Modify hostname
-sed -i 's/OpenWrt/HiWiFi4E-one/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/HiWiFi4E.Two/g' package/base-files/files/bin/config_generate
 ####### Modify the version number
 sed -i "s/OpenWrt /Fndsz build from Lienol $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/default-settings/files/zzz-default-settings
 # zerotier1.8.4
