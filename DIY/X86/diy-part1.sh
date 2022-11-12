00a8b97c7dea12cf85d2da0dc163955bf574a10c
ChenChen <cc2286927@sina.cn>
2022-11-12 13:54:25 +0800

    Update diy-part1.sh

--- DIY/X86/diy-part1.sh
+++ DIY/X86/diy-part1.sh
@@ -14,6 +14,7 @@
 git clone --depth=1 https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 git clone --depth=1 https://github.com/kenzok8/small.git package/small
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/openwrt-passwall2
 # git clone --depth=1 -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 # git clone --depth=1 https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
