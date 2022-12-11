2127a563ee0dfa5eaf5aec786018d7d0ba109be0
ChenChen <cc2286927@sina.cn>
2022-12-11 19:19:48 +0800

    Update diy-part2-hc5962.sh

--- DIY/diy-part2-hc5962.sh
+++ DIY/diy-part2-hc5962.sh
@@ -11,7 +11,7 @@ sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
 echo -e "#max-ttl=600\nneg-ttl=600\nmin-cache-ttl=3600" >> package/network/services/dnsmasq/files/dnsmasq.conf
 ###### Modify hostname
 sed -i 's/OpenWrt/HiWiFi4E.Two/g' package/base-files/files/bin/config_generate
-sed -i 's/immortalwrt/Newifi-D1/g' package/base-files/files/bin/config_generate
+sed -i 's/immortalwrt/HiWiFi4E.Two/g' package/base-files/files/bin/config_generate
 ####### Modify the Lienol version number
 #sed -i "s/OpenWrt /洲•Cy build from Lienol $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/default-settings/files/zzz-default-settings
 ####### Modify the version number
