77d711e6d0e225310f4872816b2a90bbbff1add1
ChenChen <cc2286927@sina.cn>
2023-04-28 11:14:57 +0800

    Update diy-part2-hc5962-One.sh

--- DIY/diy-part2-hc5962-One.sh
+++ DIY/diy-part2-hc5962-One.sh
@@ -6,16 +6,16 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/5.1.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/172.16.5.1/g' package/base-files/files/bin/config_generate
 # DNS Cache FixUP
 echo -e "#max-ttl=600\nneg-ttl=600\nmin-cache-ttl=3600" >> package/network/services/dnsmasq/files/dnsmasq.conf
 ###### Modify hostname
-sed -i 's/OpenWrt/HiWiFi4E-one/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/HiWiFi4E.one/g' package/base-files/files/bin/config_generate
 ####### Modify the Lienol version number
 #sed -i "s/OpenWrt /洲•Cy build from Lienol $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/default-settings/files/zzz-default-settings
 ####### Modify the version number
 sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
-echo "DISTRIB_DESCRIPTION='OpenWrt 21.02-SNAPSHOT'" >> package/base-files/files/etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='OpenWrt '" >> package/base-files/files/etc/openwrt_release
 sed -i "s/OpenWrt /洲·Cy build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/base-files/files/etc/openwrt_release
 # 修改连接数
 sed -i 's/net.netfilter.nf_conntrack_max=.*/net.netfilter.nf_conntrack_max=165535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
