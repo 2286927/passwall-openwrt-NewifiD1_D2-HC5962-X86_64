d82b7d2926a1762bd13eb17d3fa4f2271fd1df60
ChenChen <cc2286927@sina.cn>
2024-08-10 20:28:35 +0800

    Update diy-part2-d1.sh

--- DIY/diy-part2-d1.sh
+++ DIY/diy-part2-d1.sh
@@ -1,5 +1,5 @@
 # Modify default IP
-sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/172.16.10.1/g' package/base-files/files/bin/config_generate
 # DNS Cache FixUP
 echo -e "#max-ttl=600\nneg-ttl=600\nmin-cache-ttl=3600" >> package/network/services/dnsmasq/files/dnsmasq.conf
 ###### Modify hostname
